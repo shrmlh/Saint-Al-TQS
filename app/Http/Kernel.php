625610c6f7da61c2e9ceb697e147f4a622756f75
brynpryl <brynprylbrynpryl@gmail.com>
2021-08-01 20:31:20 +0800

    added middlewares for roles

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,5 +62,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'admin' => \App\Http\Middleware\Admin::class,
+        'rider' => \App\Http\Middleware\Rider::class,
+        'prevent-back-history' => \App\Http\Middleware\PreventBackHistory::class,
     ];
 }
