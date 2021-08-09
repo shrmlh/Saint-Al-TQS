df1e73ccdb403212241047155d5ae119d0051bff
brynpryl <brynprylbrynpryl@gmail.com>
2021-08-09 22:12:07 +0800

    configured forget password

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,48 +1,45 @@
 <x-guest-layout>
+    <div id="preloader">
+        <div class="loader"></div>
+    </div>
+    <!-- preloader area end -->
+    <!-- login area start -->
+    <div class="login-area">
+        <div class="container">
+            <div class="login-box ptb--100">
 
+                <form method="POST" action="{{ route('password.email') }}" class="pt-3">
+                    @csrf
+                    <div class="login-form-head">
+                        <h4>Forgot your password? No problem.</h4>
+                        <p>Just let us know your email address and we will email you a password reset link.</p>
+                    </div>
+                    <div class="login-form-body">
+                    <!-- Session Status -->
+                    <div class="text-danger">
+                        <x-auth-session-status class="pt-1" :status="session('status')" />
 
-    <div class="container-scroller">
-        <div class="container-fluid page-body-wrapper full-page-wrapper">
-            <div class="content-wrapper d-flex align-items-center auth">
-                <div class="row flex-grow">
-                    <div class="col-lg-4 mx-auto">
-                        <div class="auth-form-light text-left p-5">
-                            <div class="brand-logo">
-                                <a href="/">
-                                    <img src="{{asset('/assets/images/logo.svg')}}">
-                                </a>
-                            </div>
-                            <h4>Forgot your password? No problem.</h4>
-                            <h6 class="font-weight-light">Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.</h6>
-
-                            <!-- Session Status -->
-                            <x-auth-session-status class="mb-4" :status="session('status')" />
-
-                            <!-- Validation Errors -->
-                            <x-auth-validation-errors class="pt-2" :errors="$errors" />
-
-                            <form method="POST" action="{{ route('password.email') }}" class="pt-3">
-                                @csrf
-                                <div class="form-group">
-                                    <x-input type="email" name="email" :value="old('email')" required autofocus
-                                        class="form-control form-control-lg" id="email"
-                                        placeholder="Email"/>
-                                </div>
-                                <div class="mt-3">
-                                    <button
-                                        class="btn btn-block btn-gradient-primary btn-sm font-weight-medium auth-form-btn">
-                                        {{ __('Email Password Reset Link') }}</button>
-                                </div>
-                                <div class="text-center mt-3 font-weight-light">
-                                    Don't have an account?
-                                    @if (Route::has('register'))
-                                        <a href="{{ route('register') }}" class="text-primary">Register!</a>
-                                    @endif
-                                </div>
-                            </form>
+                        <!-- Validation Errors -->
+                        <x-auth-validation-errors class="pb-4" :errors="$errors" />
+                    </div>
+                        <div class="form-group">
+                            <label for="email">Email address</label>
+                            <x-input type="email" class="form-control" name="email" :value="old('email')" required id="email" autofocus aria-describedby="emailHelp" placeholder="Enter email"/>
+                            <small id="emailHelp" class="form-text text-muted">We'll never share your
+                                email with anyone else.</small>
+                        </div>
+                        <div class="submit-btn-area">
+                            <button id="form_submit" type="submit">Email Password Reset Link <i class="ti-arrow-right"></i></button>
+                        </div>
+                        <div class="form-footer text-center mt-2">
+                            <p class="text-muted">Don't have an account? 
+                                @if (Route::has('register'))
+                                    <a href="{{ route('register') }}">Register!</a>
+                                @endif
+                            </p>
                         </div>
                     </div>
-                </div>
+                </form>
             </div>
         </div>
     </div>
