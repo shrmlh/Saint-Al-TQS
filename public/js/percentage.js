69cccb4df0d1ab096a11f3b9239ba60567f5afd1
brynpryl <brynprylbrynpryl@gmail.com>
2021-08-16 15:08:54 +0800

    altered mystyle and percentage

--- public/js/percentage.js
+++ public/js/percentage.js
@@ -4,7 +4,8 @@ function event_progress(startDate, endDate, incrementor){
     today = new Date(), 
     p = Math.round(((today - start) / (end - start)) * 100);
     percentage = p + '%';
-    if (today>start && start<end){
+    // for progress
+    if (today>start){
         if(p<=0){
             $('#progressor'+incrementor).css("width", "0%");
         }
@@ -20,12 +21,42 @@ function event_progress(startDate, endDate, incrementor){
     }
 }
 
+function progress(regDate, startDate, endDate, progressStatus, incrementor){
+    // for status color
+    if (progressStatus === 'Open'){
+        $('#progressor'+incrementor).addClass("progress-bar-striped progress-bar-animated bg-success");
+        $('#progressStatus'+incrementor).addClass("bg-success");
+        event_progress(regDate, startDate, incrementor);
+    }
+    else if (progressStatus === 'Closed'){
+        $('#progressor'+incrementor).css("width", "100%").after().append("Closed");
+        $('#progressor'+incrementor).addClass("bg-dark");
+        $('#progressStatus'+incrementor).addClass("bg-dark");
+    }
+    else if (progressStatus === 'Ongoing'){
+        $('#progressor'+incrementor).addClass("progress-bar-striped progress-bar-animated bg-warning");
+        $('#progressStatus'+incrementor).addClass("bg-warning");
+        event_progress(startDate, endDate, incrementor);
+    }
+    else if (progressStatus === 'Cancelled'){
+        $('#progressor'+incrementor).css("width", "100%").after().append("Cancelled");
+        $('#progressor'+incrementor).addClass("bg-danger");
+        $('#progressStatus'+incrementor).addClass("bg-danger");
+    }
+    else if (progressStatus === 'Completed'){
+        $('#progressor'+incrementor).css("width", "100%").after().append("Completed");
+        $('#progressor'+incrementor).addClass("bg-primary");
+        $('#progressStatus'+incrementor).addClass("bg-primary");
+    }
+}
 
 eventCount = document.getElementById('event_count').value;
 
 for (let i = 0; i < eventCount; i++) {
+    regDate = document.getElementById('reg_start'+(i+1)).value;
     startedDate = document.getElementById('event_start'+(i+1)).value;
     endedDate = document.getElementById('event_end'+(i+1)).value;
-    event_progress(startedDate, endedDate, (i+1));
+    progStatus = document.getElementById('event_status'+(i+1)).value;
+    progress(regDate, startedDate, endedDate, progStatus, (i+1));
 }
 
