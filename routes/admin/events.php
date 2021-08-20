484ec5340c2fb9a1f9253d933e1408a63bfad74c
brynpryl <brynprylbrynpryl@gmail.com>
2021-08-20 08:49:53 +0800

    added post request

--- routes/admin/events.php
+++ routes/admin/events.php
@@ -5,3 +5,4 @@ use App\Http\Controllers\EventController;
 // Event module
 Route::get('/event-list', [EventController::class, 'index'])->name('eventsList');
 Route::get('/create-event', [EventController::class, 'create'])->name('createEvent');
+Route::post('/store-event', [EventController::class, 'store'])->name('storeEvent');
