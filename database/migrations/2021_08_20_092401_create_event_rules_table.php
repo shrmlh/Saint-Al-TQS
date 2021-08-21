ba6c105af27b3cd9a29c815b9fb1ae5960f8f095
brynpryl <brynprylbrynpryl@gmail.com>
2021-08-21 19:05:09 +0800

    added new tables

---
+++ database/migrations/2021_08_20_092401_create_event_rules_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventRulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('event_rules', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('event');
+            $table->foreign('event')->references('id')->on('events');
+            $table->string('rule',150);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('event_rules');
+    }
+}
