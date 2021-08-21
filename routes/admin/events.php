ba9f1249b67bc51bf4b8c8bdc3bd7e7c065d89f6
brynpryl <brynprylbrynpryl@gmail.com>
2021-08-21 18:37:24 +0800

    completed crud in route of event

--- routes/admin/events.php
+++ routes/admin/events.php
@@ -6,3 +6,6 @@ use App\Http\Controllers\EventController;
 Route::get('/event-list', [EventController::class, 'index'])->name('eventsList');
 Route::get('/create-event', [EventController::class, 'create'])->name('createEvent');
 Route::post('/store-event', [EventController::class, 'store'])->name('storeEvent');
+Route::get('/edit-event/{event}/edit', [EventController::class, 'edit'])->name('editEvent');
+Route::put('/update-event/{event}', [EventController::class, 'update'])->name('updateEvent');
+Route::delete('/delete-event/{event}', [EventController::class, 'destroy'])->name('deleteEvent');
