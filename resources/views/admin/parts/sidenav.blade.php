60803763576a4b4ae0f8022ac0d11d7f5cc78ec8
brynpryl <brynprylbrynpryl@gmail.com>
2021-08-30 21:38:08 +0800

    changed icons

--- resources/views/admin/parts/sidenav.blade.php
+++ resources/views/admin/parts/sidenav.blade.php
@@ -8,16 +8,16 @@
                 <div class="menu-inner">
                     <nav class="sidebar">
                         <ul class="metismenu" id="menu">
-                            <li class="nav-item"><a href="{{ route('admin') }}"><i class="ti-map-alt"></i> <span>Dashboard</span></a></li>
+                            <li class="nav-item"><a href="{{ route('admin') }}"><i class="ti-home"></i> <span>Dashboard</span></a></li>
                             <li class="nav-item">
-                                <a href="javascript:void(0)" aria-expanded="false"><i class="ti-dashboard"></i><span>Events</span></a>
+                                <a href="javascript:void(0)" aria-expanded="false"><i class="ti-calendar"></i><span>Events</span></a>
                                 <ul class="collapse sub-menu">
                                     <li class="nav-item"><a href="{{ route('eventsList') }}">List of Events</a></li>
                                     <li class="nav-item"><a href="{{ route('createEvent') }}">Create Event</a></li>
                                 </ul>
                             </li>
                             <li>
-                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-pie-chart"></i><span>Payment</span></a>
+                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-money"></i><span>Payment</span></a>
                                 <ul class="collapse">
                                     <li><a href="barchart.html">Membership Applications</a></li>
                                     <li><a href="linechart.html">Event Applications</a></li>
@@ -25,28 +25,28 @@
                                 </ul>
                             </li>
                             <li class="nav-item">
-                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-slice"></i><span>Riders</span></a>
+                                <a href="javascript:void(0)" aria-expanded="true"><i class="fa fa-motorcycle"></i><span>Riders</span></a>
                                 <ul class="collapse sub-menu">
                                     <li class="nav-item"><a href="{{ route('userRiderList') }}">Riders List</a></li>
-                                    <li class="nav-item"><a href="themify.html">Rider Management</a></li>
+                                    <li class="nav-item"><a href="themify.html">Rider Groups</a></li>
                                 </ul>
                             </li>
                             <li>
-                                <a href="javascript:void(0)" aria-expanded="true"><i class="fa fa-table"></i>
+                                <a href="javascript:void(0)" aria-expanded="true"><i class="fa fa-calendar-check-o"></i>
                                     <span>Attendance</span></a>
                                 <ul class="collapse">
                                     <li><a href="table-basic.html">Participation</a></li>
                                 </ul>
                             </li>
                             <li class="nav-item">
-                                <a href="javascript:void(0)" aria-expanded="true"><i class="fa fa-table"></i>
+                                <a href="javascript:void(0)" aria-expanded="true"><i class="fa fa-users"></i>
                                     <span>Users</span></a>
                                 <ul class="collapse sub-menu">
                                     <li class="nav-item"><a href="{{route('userAdminList')}}">User List</a></li>
                                     <li class="nav-item"><a href="{{route('createUserAdmin')}}">New User</a></li>
                                 </ul>
                             </li>
-                            <li><a href="maps.html"><i class="ti-map-alt"></i> <span>Backup and Restore</span></a></li>
+                            <li><a href="maps.html"><i class="fa fa-database"></i> <span>Backup and Restore</span></a></li>
                         </ul>
                     </nav>
                 </div>
